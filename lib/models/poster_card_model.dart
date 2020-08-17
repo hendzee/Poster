d570b10188c03f3851a27e1dec8aaecc615a8fff
Virginia Hendras <hendzcode@gmail.com>
2020-08-17 12:58:45 +0700

    :lipstick: Updating home screen UI

---
+++ lib/models/poster_card_model.dart
@@ -0,0 +1,15 @@
+class PosterCardModel {
+  final String title;
+  final String date;
+  final String location;
+  final String description;
+  final String posterImage;
+
+  PosterCardModel({
+    this.title,
+    this.date,
+    this.location,
+    this.description,
+    this.posterImage
+  });
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:eva_icons_flutter/eva_icons_flutter.dart';
+import 'package:poster/models/poster_card_model.dart'; // Poster card model
+import '../widgets/general/poster_card.dart'; // Poster card widget
 import '../widgets/home_screen/top_content.dart'; // Substance of home screen
 import '../widgets/home_screen/trending_title_content.dart'; // Sunstance of home screen
 
@@ -11,6 +12,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // This is dummy data for this page
+  final PosterCardModel posterCardModel1 = new PosterCardModel(
+    title: 'Harmony Concert 2020',
+    date: '20 Sep, 2020',
+    location: 'Embong anyar Street no 10, Malang',
+    description: 'Ipsum is simply dummy of the printing and typesetting industry. Lorem Ipsum',
+    posterImage: 'assets/dummy_images/poster1.png'
+  );
+
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
@@ -25,117 +35,27 @@ class _HomeScreenState extends State<HomeScreen> {
             children: <Widget>[
               TopContent(),
               TrendingTitleContent(),
+              PosterCard(posterCardModel: this.posterCardModel1,),
               Container(
-                margin: EdgeInsets.only(left: 25.0, right: 25.0, top: 10.0, bottom: 25.0),
-                width: double.infinity,
-                height: 220,
-                child: Stack(
-                  fit: StackFit.expand,
-                  children: <Widget>[
-                    Positioned.fill(
-                      child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Wrap(
-                          children: <Widget>[
-                            Container(
-                              width: double.infinity,
-                              height: 190,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Colors.grey.withOpacity(0.5),
-                                    spreadRadius: 5.0,
-                                    blurRadius: 7.0,
-                                    offset: Offset(0, 3)
-                                  )
-                                ]
-                              ),
-                              child: Stack(
-                                children: <Widget>[
-                                  Positioned(
-                                    right: 0,
-                                    child: Container(
-                                      padding: EdgeInsets.all(5.0),
-                                      width: 160,
-                                      height: 180,
-                                      child: Column(
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: <Widget>[
-                                          Padding(
-                                            padding: const EdgeInsets.only(bottom: 10.0),
-                                            child: Text('Concert with James',
-                                              style: TextStyle(fontWeight: FontWeight.bold),
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: const EdgeInsets.only(bottom: 5.0),
-                                            child: Wrap(
-                                              children: <Widget>[
-                                                Icon(
-                                                  EvaIcons.calendarOutline,
-                                                  size: 13.0,
-                                                ),
-                                                SizedBox(width: 5,),
-                                                Text('20 Sep, 2020',
-                                                  style: TextStyle(fontSize: 12.0),
-                                                )
-                                              ],
-                                            ),
-                                          ),Padding(
-                                            padding: const EdgeInsets.only(bottom: 5.0),
-                                            child: Wrap(
-                                              children: <Widget>[
-                                                Icon(
-                                                  EvaIcons.pinOutline,
-                                                  size: 13.0,
-                                                ),
-                                                SizedBox(width: 5,),
-                                                Text('Sesame street no.10...',
-                                                  style: TextStyle(fontSize: 12.0),
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                          Padding(
-                                            padding: EdgeInsets.only(top: 10.0, bottom: 5.0),
-                                            child: Text('Desc',
-                                              style: TextStyle(
-                                                fontSize: 12.0,
-                                                fontWeight: FontWeight.bold,
-                                              ),
-                                            ),
-                                          ),
-                                          Wrap(
-                                            children: <Widget>[
-                                              Text('Ipsum is simply dummy of the printing and typesetting industry. Lorem Ipsum ...',
-                                                style: TextStyle(fontSize: 12.0),
-                                              )
-                                            ],
-                                          )
-                                        ],
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      bottom: 15,
-                      left: 15,
-                      width: 135,
-                      child: Container(
-                        height: 200,
-                        color: Colors.grey,
-                        child: Text('Image'),
-                      ),
-                    ),
-                  ],
+                padding: EdgeInsets.all(25.0),
+                decoration: BoxDecoration(
+                  color: Colors.redAccent,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                  )
+                ),
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      Text('Recomended'),
+                      Text('This is item 2'),
+                      Text('This is item 3'),
+                      Text('This is item 4'),
+                      Text('This is item 5'),
+                      Text('This is item 6'),
+                    ],
+                  ),
                 ),
               )
             ],
