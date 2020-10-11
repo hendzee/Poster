76a48d8e577cde53f8e8099e0936020075edee3f
Virginia Hendras <hendzcode@gmail.com>
2020-10-11 13:24:42 +0700

    :lipstick: Updating login input label UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -18,114 +18,147 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 650,
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(horizontal: 25),
-            decoration: BoxDecoration(
+        body: SafeArea(
+      child: SingleChildScrollView(
+        child: Container(
+          height: 650,
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
               color: Color(0xFF40407A),
-              borderRadius: BorderRadius.only(bottomRight: Radius.circular(60))
-            ),
-            child: Column(
-              children: [
-                SizedBox(
-                  height: 80,
-                ),
-                Text('Login', style:
-                  TextStyle(color: Colors.white, fontSize: 36, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(height: 25,),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      TextField(
-                        style: TextStyle(color: Color(0xFF7171A6)),
-                        decoration: InputDecoration(
-                          enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                          ),
-                          focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                          ),
-                          prefixIcon: Icon(EvaIcons.emailOutline, color: Colors.white,),
-                          labelText: 'Email',
-                          labelStyle: TextStyle(color: Colors.white)
+              borderRadius:
+                  BorderRadius.only(bottomRight: Radius.circular(60))),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 80,
+              ),
+              Text(
+                'Login',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextField(
+                      style: TextStyle(color: Color(0xFF7171A6)),
+                      decoration: InputDecoration(
+                        enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white)),
+                        prefixIcon: Icon(
+                          EvaIcons.emailOutline,
+                          color: Colors.white,
+                        ),
+                        labelText: 'Email',
+                        labelStyle: TextStyle(
+                          color: Color(0xFFABABD3),
                         ),
                       ),
-                      SizedBox(height: 25,),
-                      TextField(
-                        obscureText: _isPasswordSecure,
-                        style: TextStyle(color: Color(0xFF7171A6)),
-                        decoration: InputDecoration(
+                    ),
+                    SizedBox(
+                      height: 25,
+                    ),
+                    TextField(
+                      obscureText: _isPasswordSecure,
+                      style: TextStyle(color: Color(0xFF7171A6)),
+                      decoration: InputDecoration(
                           enabledBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
-                          ),
+                              borderSide: BorderSide(color: Colors.white)),
                           focusedBorder: UnderlineInputBorder(
-                            borderSide: BorderSide(color: Colors.white)
+                              borderSide: BorderSide(color: Colors.white)),
+                          prefixIcon: Icon(
+                            EvaIcons.personOutline,
+                            color: Colors.white,
                           ),
-                          prefixIcon: Icon(EvaIcons.personOutline, color: Colors.white,),
                           suffixIcon: IconButton(
                             icon: Icon(
-                              _isPasswordSecure ? EvaIcons.eyeOff2Outline : EvaIcons.eyeOutline,
+                              _isPasswordSecure
+                                  ? EvaIcons.eyeOff2Outline
+                                  : EvaIcons.eyeOutline,
                               color: Colors.white,
                             ),
-                            onPressed: (){
+                            onPressed: () {
                               this.setState(() {
-                                _isPasswordSecure = _isPasswordSecure ? false : true;
+                                _isPasswordSecure =
+                                    _isPasswordSecure ? false : true;
                               });
                             },
                           ),
                           labelText: 'Password',
-                          labelStyle: TextStyle(color: Colors.white)
-                        ),
+                          labelStyle: TextStyle(color: Color(0xFFABABD3))),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'Forgot Password',
+                        style: TextStyle(color: Color(0xFFD14081)),
                       ),
-                      SizedBox(height: 10,),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: Text('Forgot Password', style: TextStyle(color: Color(0xFFD14081)),),
-                      ),
-                      SizedBox(height: 50,),
-                      GestureDetector(
-                        onTap: () => _handleLogin(context),
-                        child: Container(
-                          width: double.infinity,
-                          height: 50,
-                          decoration: BoxDecoration(
-                            color: Color(0xFFD14081),
-                            borderRadius: BorderRadius.all(Radius.circular(10))
-                          ),
-                          child: Center(
-                            child: Text('Login', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: EdgeInsets.symmetric(vertical: 40),
-                        child: Text('Dont\' have account yet ?', style: TextStyle(color: Colors.white),),
-                      ),
-                      Container(
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    GestureDetector(
+                      onTap: () => _handleLogin(context),
+                      child: Container(
                         width: double.infinity,
                         height: 50,
                         decoration: BoxDecoration(
-                            color: Color(0xFF7171A6),
-                            borderRadius: BorderRadius.all(Radius.circular(10))
-
-                        ),
+                            color: Color(0xFFD14081),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10))),
                         child: Center(
-                          child: Text('Sign Up', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),),
+                          child: Text(
+                            'Login',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 18),
+                          ),
                         ),
                       ),
-                    ],
-                  ),
-                )
-              ],
-            ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(vertical: 40),
+                      child: Text(
+                        'Dont\' have account yet ?',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      width: double.infinity,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          color: Color(0xFF7171A6),
+                          borderRadius: BorderRadius.all(Radius.circular(10))),
+                      child: Center(
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
-      )
-    );
+      ),
+    ));
   }
 }
