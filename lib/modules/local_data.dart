6701488a0d07405ee911692b3be23fbf5c8aedd4
Virginia Hendras <hendzcode@gmail.com>
2021-01-12 14:12:01 +0700

    Add local data helper

---
+++ lib/modules/local_data.dart
@@ -0,0 +1,49 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import '../data/models/user_model.dart';
+
+class LocalData {
+  /// Set user data to local storage
+  static void setUserLocalData(UserModel user) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    prefs.setString('id', user.id);
+    prefs.setString('firstName', user.firstName);
+    prefs.setString('lastName', user.lastName);
+    prefs.setString('email', user.email);
+    prefs.setString('phone', user.phone);
+    prefs.setString('password', user.password);
+  }
+
+  /// Get user data from local storage
+  static Future<UserModel> getUserLocalData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    UserModel user = UserModel(
+      id: prefs.getString('id'),
+      firstName: prefs.getString('firstName'),
+      lastName: prefs.getString('lastName'),
+      email: prefs.getString('email'),
+      phone: prefs.getString('phone'),
+      password: prefs.getString('password'),
+    );
+
+    return user;
+  }
+
+  /// Set login flag data to local storage
+  static void setLoginLocalData(bool status) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isLogin', status);
+  }
+
+  /// Get login flag data to local storage
+  static Future<bool> getLoginLocalData() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    if (prefs.getBool('isLogin') == null) {
+      return false;
+    }
+
+    return true;
+  }
+}
