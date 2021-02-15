d1ac245f4ad540acaa8012107e9a0da6deb38bb8
Virginia Hendras <hendzcode@gmail.com>
2021-02-15 23:03:22 +0700

    Add decoration for bottom button widget

--- lib/widgets/general/bottom_button.dart
+++ lib/widgets/general/bottom_button.dart
@@ -2,23 +2,25 @@ import 'package:flutter/material.dart';
 
 class BottomButton extends StatelessWidget {
   final String title;
+  final Function onTap;
 
-  BottomButton({ this.title });
+  BottomButton({this.title, this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Ink(
       height: 50,
       width: double.infinity,
       decoration: BoxDecoration(
           color: Color(0xFFD14081),
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20)
-          )
-      ),
-      child: Center(
-        child: Text(this.title,
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          borderRadius: BorderRadius.only(topLeft: Radius.circular(20))),
+      child: InkWell(
+        onTap: onTap,
+        child: Center(
+          child: Text(
+            this.title,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
         ),
       ),
     );
